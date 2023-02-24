9b5c6937f7a8e1eb46b3dd5ec02edaca45440003
Taras Keryk <taras.keryk@plvision.eu>
2023-02-24 15:33:16 +0200

    Redis starting using start script, checked saiserver run
    Signed-off-by: Taras Keryk <taras.keryk@plvision.eu>

--- npu/broadcom/BCM56850/saivs/Dockerfile.thrift
+++ npu/broadcom/BCM56850/saivs/Dockerfile.thrift
@@ -2,6 +2,38 @@ FROM sc-thrift-base
 
 MAINTAINER andriy.kokhan@gmail.com
 
+# Install patch for SAI check
+COPY patches/SAI/0001-improve-enum-values-integration-check-1727.patch /sai/
+
+RUN git clone https://github.com/sonic-net/sonic-sairedis.git \
+        && cd sonic-sairedis \
+        && . /sai.env \
+        && git checkout ${SAIREDIS_ID} \
+        && git submodule update --init --recursive \
+        && cd SAI && git fetch origin \
+        && git checkout ${SAI_ID} \
+        && git submodule update --init --recursive \
+        && cd .. \
+        && ./autogen.sh \
+        && dpkg-buildpackage -us -uc -b --target=binary-syncd-vs --jobs=auto \
+        && cd .. \
+        && dpkg -i libsaivs_1.0.0_amd64.deb \
+        && dpkg -i libsaivs-dev_1.0.0_amd64.deb \
+        && dpkg -i libsairedis_1.0.0_amd64.deb \
+        && dpkg -i libsairedis-dev_1.0.0_amd64.deb \
+        && dpkg -i libsaimetadata_1.0.0_amd64.deb \
+        && dpkg -i libsaimetadata-dev_1.0.0_amd64.deb \
+        && dpkg -i syncd-vs_1.0.0_amd64.deb \
+        && cp /sai/0001-improve-enum-values-integration-check-1727.patch /sai/sonic-sairedis \
+        && cd sonic-sairedis && patch -p1 < 0001-improve-enum-values-integration-check-1727.patch \
+        && cd SAI && make saithrift-install \
+        && cp meta/saimetadatautils.c /sai/gen_attr_list/ \
+        && cp meta/saimetadata.c /sai/gen_attr_list/ \
+        && cp meta/saiserialize.c /sai/gen_attr_list/ \
+        && mv /sai/sonic-sairedis/tests /sai/ \
+        && rm -rf /sai/sonic-sairedis/* \
+        && mv /sai/tests /sai/sonic-sairedis/
+
 # Setup supervisord
 COPY configs/sai.profile       /etc/sai.d/sai.profile
 COPY configs/lanemap.ini       /usr/share/sonic/hwsku/lanemap.ini
