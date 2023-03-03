2a456900a9cb9621fe6cd45d2ac2e317c30193c7
Taras Keryk <taras.keryk@plvision.eu>
2023-03-03 11:42:13 +0200

    Fixed review comments
    Signed-off-by: Taras Keryk <taras.keryk@plvision.eu>

--- usecases/sai-ptf/ptf-conftest.py
+++ usecases/sai-ptf/ptf-conftest.py
@@ -84,12 +84,6 @@ config_default = {
     "port_map": None,
 }
 
-def import_base_modules():
-    sys.path.insert(0, '/sai-challenger/ptf/src')
-
-import_base_modules()
-
-
 @pytest.fixture(scope="session", autouse=True)
 def set_ptf_params(request):
     if request.config.option.testbed:
@@ -99,8 +93,6 @@ def set_ptf_params(request):
         ports = ""
     
     # provide required PTF runner params to avoid exiting with an error
-    # sys.argv = ['ptf.py','--test-dir', '/sai-challenger/usecases/sai-ptf/SAI/ptf', *ports]
-    # import_base_modules()
 
     # load PTF runner module to let it collect test params into ptf.config
     import imp
@@ -110,7 +102,6 @@ def set_ptf_params(request):
 
     ptf.config.update(config_default)
     logging_setup(config)
-    xunit_setup(config)
     logging.info("++++++++ " + time.asctime() + " ++++++++")
 
     # import after logging is configured so that scapy error logs (from importing
@@ -120,7 +111,6 @@ def set_ptf_params(request):
     # Try parsing test params and log them
     # We do this before importing the test modules in case test parameters are being
     # accessed at test import time.
-    #ptf.testutils.TEST_PARAMS = test_params_parse(config)
 
     # Initiallize port information
     ptf.testutils.PORT_INFO = config["port_info"]
@@ -136,13 +126,6 @@ def set_ptf_params(request):
     # Load the platform module
     platform_name = config["platform"]
     logging.info("Importing platform: " + platform_name)
-
-    if platform_name == "nn":
-        try:
-            import nnpy
-        except:
-            logging.critical("Cannot use 'nn' platform if nnpy package is not installed")
-            sys.exit(1)
 
     platform_mod = None
     try:
@@ -170,27 +153,23 @@ def set_ptf_params(request):
     ptf.ptfutils.default_negative_timeout = config["default_negative_timeout"]
     ptf.testutils.MINSIZE = config["minsize"]
 
-    if os.getuid() != 0 and not config["allow_user"] and platform_name != "nn":
+    if os.getuid() != 0 and not config["allow_user"]:
         logging.critical("Super-user privileges required. Please re-run with sudo or as root.")
         sys.exit(1)
 
-    if config["random_seed"] is not None:
-        logging.info("Random seed: %d" % config["random_seed"])
-        random.seed(config["random_seed"])
-    else:
-        # Generate random seed and report to log file
-        seed = random.randrange(100000000)
-        logging.info("Autogen random seed: %d" % seed)
-        random.seed(seed)
+    seed = config["random_seed"] if config["random_seed"] else random.randrange(100000000)
+    logging.info("Random seed: %d" % seed)
+    random.seed(seed)
 
     # Remove python's signal handler which raises KeyboardError. Exiting from an
     # exception waits for all threads to terminate which might not happen.
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-    #---------------------------------
     # Set up the dataplane
     ptf.dataplane_instance = ptf.dataplane.DataPlane(config)
-    pcap_setup(config)
+    if config["log_dir"] == None:
+        filename = os.path.splitext(config["log_file"])[0] + ".pcap"
+        ptf.dataplane_instance.start_pcap(filename)
 
     for port_id, ifname in config["port_map"].items():
         device, port = port_id
@@ -228,29 +207,3 @@ def logging_setup(config):
             os.remove(config["log_file"])
 
     ptf.open_logfile("main")
-
-def xunit_setup(config):
-    """
-    Set up xUnit output based on config
-    """
-
-    if not config["xunit"]:
-        return
-
-    if os.path.exists(config["xunit_dir"]):
-        import shutil
-
-        shutil.rmtree(config["xunit_dir"])
-    os.makedirs(config["xunit_dir"])
-
-def pcap_setup(config):
-    """
-    Set up dataplane packet capturing based on config
-    """
-
-    if config["log_dir"] == None:
-        filename = os.path.splitext(config["log_file"])[0] + ".pcap"
-        ptf.dataplane_instance.start_pcap(filename)
-    else:
-        # start_pcap is called per-test in base_tests
-        pass
